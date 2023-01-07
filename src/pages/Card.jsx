5a64f83dd48535c8176f0fed03cd91dbddcf281c
hanthuthuhtet <htth5499@gmail.com>
2023-01-07 23:53:42 +0630

    details page

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-
+import {AiFillEye} from "react-icons/ai"
+import {Link} from "react-router-dom"
 const Card = ({meal}) => {
   return (
-    <div className='w-72 border-2 flex justify-center items-center flex-col gap-5 p-5 rounded-lg'>
+    <div className='w-60 border-2 flex justify-center items-center flex-col gap-5 p-5 rounded-lg relative h-[400px] shadow hover:scale-105 transition duration-200 hover:shadow-lg '>
         <img src={meal.strMealThumb} width="200px" className='rounded-lg' alt="" />
         <h3>{meal.strMeal}</h3>
-        <button className='text-whie bg-blue-500 px-10 py-2 rounded-md'>Detail</button>
+        <Link to={`/detail/${meal.idMeal}`}>
+          <button className='text-white bg-blue-500 
+          px-8 py-2  rounded-md  bottom-3 flex'>Details<AiFillEye className='text-2xl pl-1' /></button>
+        </Link>
     </div>
   )
 }
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import axios from "axios"
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const Detail = () => {
+  const {id} = useParams();
+  const [meal , setMeal ] = useState({});
+  const getSingleMeal = async() => {
+    const {data} = await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+    console.log(data.meals);
+    setMeal(data?.meals[0]);
+  }
+  useEffect(() => {
+    getSingleMeal();
+  },[])
   return (
-    <div>Detail</div>
+    <div>
+      <img src={meal.strMealThumb} alt="" width={"300px"} className="rounded-xl shadow" />
+      <div>{meal.strCategory}</div>
+      <h1>{meal.strMeal}</h1>
+      <div>
+        <h2>Instruction</h2>
+        <p>{meal.strInstructions}</p>
+      </div>
+    </div>
   )
 }
 
